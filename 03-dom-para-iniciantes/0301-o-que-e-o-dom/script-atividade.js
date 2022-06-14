fc7d205a35b576ab8f17237c312defb81d28cd05
viniciusBTT <vini.barrost@gmail.com>
2022-06-14 17:13:14 -0300

    exercicio dom 01

---
+++ 03-dom-para-iniciantes/0301-o-que-e-o-dom/script-atividade.js
@@ -0,0 +1,15 @@
+// Retorne o url da página atual utilizando o objeto window
+let href = window.location.href;
+console.log(href);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+let h1 = document.querySelector
+
+// Retorne a linguagem do navegador
+
+
+
+// Retorne a largura da janela 
+let largura = window.innerWidth;
+console.log(largura)
---
+++ 03-dom-para-iniciantes/0301-o-que-e-o-dom/script-notas.js
@@ -0,0 +1,21 @@
+
+
+let href = window.location.href;
+
+console.log(href);
+
+function confere(){
+  if ( href == 'http://127.0.0.1:5500/03-dom-para-iniciantes/0301-o-que-e-o-dom/'){
+    console.log('É igual');
+  }
+}
+
+/////////////////////////////////////////////
+const h1Selecionado = document.querySelector('h1');
+const h2Selecionado = document.querySelector('h2');
+
+function callbackH1(){  
+  console.log('Clicou em' , h1Selecionado.innerText );
+}
+
+h1Selecionado.addEventListener('click', callbackH1);
