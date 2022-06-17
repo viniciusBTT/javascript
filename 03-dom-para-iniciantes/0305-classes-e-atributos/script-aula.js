4102ada71e543e6754c845923e6e962ebf7b32b0
viniciusBTT <vini.barrost@gmail.com>
2022-06-17 14:26:56 -0300

    att

--- 03-dom-para-iniciantes/0305-classes-e-atributos/script-aula.js
+++ 03-dom-para-iniciantes/0305-classes-e-atributos/script-aula.js
@@ -4,7 +4,16 @@ menu.classList.remove('azul')
 menu.classList.toggle('ativo')
 menu.classList.toggle('azul')//add ou remove a classe
 console.log(menu.classList);
-//classList.contains - verifica se possui a propriedade 
+//classList.contains( ) - verifica se possui a propriedade 
 if(menu.classList.contains('azul')){
-  menu.classList.add('possui-azul')
+  menu.classList.add('possui-azul');
 }
+
+console.log(menu.className);
+//antiga forma de add ou remover classe menu.className. retorna uma string com os nomes das classes
+
+const animais = document.querySelector('.animais');
+console.log(animais.attributes['data-texto']);
+
+const img = document.querySelector('img');
+console.log(img.hasAttribute('alt'));
